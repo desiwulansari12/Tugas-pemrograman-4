b192a4828a26d788a4c559234fe0f8436358cca6
desiwulansari12 <desi.wulanssari@gmail.com>
2025-03-13 22:39:24 +0700

    Delete # 1. Jenis-Jenis operator pada pyhton.py
    menghapus

--- # 1. Jenis-Jenis operator pada pyhton.py
+++
@@ -1,7 +0,0 @@
-# Jenis-Jenis operator pada pyhton
-# 1. operator arimatika = Operator aritmatika adalah operator yang digunakan dengan nilai numerik untuk melakukan operasi matematika umum. misalnya pertamabahan, penguragan, pembagian)
-
-
-
-print ("Jenis-Jenis operator pada pyhton")
-print ("1. operator arimatika")
